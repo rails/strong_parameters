384fb6a9d8d85d13585a7b0ceb53a1cd470451a8
Aaron Patterson <aaron.patterson@gmail.com>
2012-03-19 14:15:25 -0700

    use Ruby to avoid AMC

--- lib/active_model/mass_assignment_taint_protection.rb
+++ lib/active_model/mass_assignment_taint_protection.rb
@@ -1,19 +1,13 @@
 module ActiveModel
   class TaintedAttributes < StandardError
   end
-  
+
   module MassAssignmentTaintProtection
-    extend ActiveSupport::Concern
-    
-    included do
-      alias_method_chain :sanitize_for_mass_assignment, :taint_checking
-    end
-    
-    def sanitize_for_mass_assignment_with_taint_checking(new_attributes, options = {})
+    def sanitize_for_mass_assignment(new_attributes, options = {})
       if new_attributes.tainted?
         raise ActiveModel::TaintedAttributes
       else
-        sanitize_for_mass_assignment_without_taint_checking(new_attributes, options)
+        super
       end
     end
   end
