6e86d12fc76e2bc34622c95dc0853f00c0a70c7d
Aaron Patterson <aaron.patterson@gmail.com>
2012-03-19 14:15:02 -0700

    use IndexError to be closer to hash fetch

--- lib/action_controller/parameters.rb
+++ lib/action_controller/parameters.rb
@@ -1,7 +1,7 @@
 require 'active_support/core_ext/hash/indifferent_access'
 
 module ActionController
-  class ParameterMissing < RuntimeError
+  class ParameterMissing < IndexError
   end
 
   class Parameters < ActiveSupport::HashWithIndifferentAccess
