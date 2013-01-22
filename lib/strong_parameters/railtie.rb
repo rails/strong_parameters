11332b95133b6e443b0b1ae5c92ade9b7354c11f
Thomas Drake-Brockman <thom@sfedb.com>
2013-01-23 07:42:57 +0800

    Raise or log with when unpermitted params are provided. Ported from rails/rails.

--- lib/strong_parameters/railtie.rb
+++ lib/strong_parameters/railtie.rb
@@ -7,5 +7,11 @@ module StrongParameters
     else
       config.generators.scaffold_controller = :strong_parameters_controller
     end
+
+    initializer "strong_parameters.config", :before => "active_controller.set_configs" do |app| 
+      ActionController::Parameters.action_on_unpermitted_parameters = options.delete(:action_on_unpermitted_parameters) do
+        (Rails.env.test? || Rails.env.development?) ? :log : false
+      end
+    end
   end
 end
