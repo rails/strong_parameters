c6ba4f937a71132f0df45387500c6b65a5610bbc
Sebastian Sogamoso <sebasoga@gmail.com>
2013-03-09 15:56:36 -0500

    Change ActionController::Parameters#require behavior when value is empty When the value for the required key is empty an ActionController::ParameterMissing is raised which gets caught by ActionController::Base and turned into a 400 Bad Request reply with a message in the body saying the key is missing, which is misleading.
    With these changes, ActionController::EmptyParameter will be raised which ActionController::Base will catch and turn into a 400 Bad Request reply with a message in the body saying the key value is empty.

--- test/parameters_require_test.rb
+++ test/parameters_require_test.rb
@@ -2,9 +2,23 @@ require 'test_helper'
 require 'action_controller/parameters'
 
 class ParametersRequireTest < ActiveSupport::TestCase
-  test "required parameters must be present not merely not nil" do
+  test "required parameters must be present" do
     assert_raises(ActionController::ParameterMissing) do
+      ActionController::Parameters.new(:name => {}).require(:person)
+    end
+  end
+
+  test "required parameters can't be blank" do
+    assert_raises(ActionController::EmptyParameter) do
       ActionController::Parameters.new(:person => {}).require(:person)
+    end
+
+    assert_raises(ActionController::EmptyParameter) do
+      ActionController::Parameters.new(:person => '').require(:person)
+    end
+
+    assert_raises(ActionController::EmptyParameter) do
+      ActionController::Parameters.new(:person => nil).require(:person)
     end
   end
 end
